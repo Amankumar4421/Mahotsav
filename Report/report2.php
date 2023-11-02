b3e5c476f143e30e5ffe0bb555144ed30c754e32
Amankumar4421 <kumaraman111115@gmail.com>
2023-11-02 14:08:52 +0530

    report updated

--- Report/report2.php
+++ Report/report2.php
@@ -62,11 +62,22 @@ th,td{
               while ($row1 = mysqli_fetch_assoc($result1)) {
 
                 $college = $row1['clg'];
-                echo "<tr><td>".$college.", ";
+                echo "<tr><td>".$college."<br> ";
                 $s = "SELECT state,district FROM `student` where college='$college'";
                 $r = mysqli_query($con, $s);
                 $r1 = mysqli_fetch_assoc($r);
-                echo $r1['district'].", ".$r1['state']."</td>";
+                $dist = $r1['district'];
+                $state = $r1['state'];
+
+                $s = "SELECT name FROM `district` where no='$dist'";
+                $r = mysqli_query($con, $s);
+                if($r1 = mysqli_fetch_assoc($r))
+                  echo $r1['name'].", ";
+
+                $s = "SELECT name FROM `state` where no='$state'";
+                $r = mysqli_query($con, $s);
+                if($r1 = mysqli_fetch_assoc($r))
+                  echo $r1['name']."</td>";
 
                 for ($i = 1; $i <= $totalEvents; $i++) {
                   // fetch the total
