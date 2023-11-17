31d8cfecff107aeac7888a2df2260d15ad31b799
surajpatel45 <surajpatel.cse@gmail.com>
2023-11-17 10:57:32 +0530

    ...

---
+++ cricket/data.php
@@ -0,0 +1,47 @@
+<?php
+
+include("connection.php");
+
+
+
+
+// Check if the 'mahotsavID' parameter was sent via POST
+
+if (isset($_GET['mahotsavid'])) {
+    $mahotsavid = $_GET['mahotsavid'];
+    $idcount=0;
+    // Create a SQL query to fetch the name based on mahotsavid
+    $sql = "SELECT * FROM student WHERE sno = '$mahotsavid'";
+ $sql2="SELECT * FROM cricteam where mhid='$mahotsavid'";
+    $result = $con->query($sql);
+    $result2 = $con->query($sql2);
+   
+    if($result2->num_rows>0){
+        $idcount=1;
+    }
+    
+    
+    if ($result->num_rows > 0) {
+        // Output the name as plain text
+        
+        $row = $result->fetch_assoc();
+        $row['alr']=$idcount;
+       // echo $row['name'];
+       echo json_encode($row);
+    } else {
+        // Return a message if no matching record is found
+        echo "Name not found";
+    }
+}
+//  else {
+//     // Return an error message if mahotsavid is not provided
+//     echo "Mahotsavid not provided";
+// }
+
+
+// function submit($mysqli){
+// $res="insert into teamreg values($eamreg, "
+// }
+
+
+?>
