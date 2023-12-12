6a8d3bd1aff60d416cbcacc0f5df595e359ecee7
surajpatel45 <surajpatel.cse@gmail.com>
2023-12-12 11:39:20 +0530

    student page

--- student/stdform.php
+++ student/stdform.php
@@ -1,29 +1,28 @@
 <?php
 
 session_start();
-if(!isset($_SESSION['stdreg'])){
+if (!isset($_SESSION['stdreg'])) {
     header("Location:index.php");
 }
 $stdreg = $_SESSION['stdreg'];
 
 include("connection.php");
-$paystatus=0;
+$paystatus = 0;
 
 
-$str1 = "select * from student where regno='". $stdreg ."'";
+$str1 = "select * from student where regno='" . $stdreg . "'";
 
 $results = mysqli_query($con, $str1);
 $result = mysqli_fetch_assoc($results);
 
-$pstring = "select * from payment where stdreg='". $stdreg ."'";
+$pstring = "select * from payment where stdreg='" . $stdreg . "'";
 $presults = mysqli_query($con, $pstring);
 $presult = mysqli_fetch_assoc($presults);
-if(isset($presult)){
-$paystatus=1;
-$pay = "PAID";
-}
-else{
-    $pay="NOT PAID";
+if (isset($presult)) {
+    $paystatus = 1;
+    $pay = "PAID";
+} else {
+    $pay = "NOT PAID";
 }
 
 // $strin = "<input type='button' value='Access' onclick='getpass()'>";
@@ -40,85 +39,86 @@ echo '<!DOCTYPE html>
 </head>
 <body style="background-color:#fff6c9;display:flex;flex-direction:column;align-items:center;">
 <div class="whole">
+    <div id="text">
+            <div style="font-size:140%;">Student Details</div> 
+            <input type="button" id="butto" onclick="exit()" style="font-family:Arial, Helvetica, sans-serif;font-size:120%" value="Logout" ">
+    </div>
     <div class="detail">
-        <div id="text">
-            <div style="font-size:160%;">Student Details</div> 
-            <input type="button" id="butto" onclick="exit()" style="font-family:Arial, Helvetica, sans-serif;font-size:140%" value="Logout" ">
-        </div>
         <div class="info">
-            <h4> Reg No  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;:&nbsp;&nbsp;'. $result["regno"].' </h4><br>
-            <h4> Unq ID &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;:&nbsp;&nbsp;'.$result["sno"].'</h4><br>
-            <h4> Name &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;:&nbsp;&nbsp;'. $result["name"].' </h4><br>
-            <h4> DOB &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;:&nbsp;&nbsp;'.$result["dob"].'</h4><br>
-            <h4> College &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;:&nbsp;&nbsp;'.$result["college"].'</h4><br>
-            <h4> Branch &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;:&nbsp;&nbsp;'.$result["branch"].'</h4><br> 
-            <h4> PayStatus &nbsp;&nbsp;:&nbsp;&nbsp;'.$pay.'</h4><br> 
+            <h4> Reg No  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;:&nbsp;&nbsp;' . $result["regno"] . ' </h4><br>
+            <h4> Unq ID &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;:&nbsp;&nbsp;' . $result["sno"] . '</h4><br>
+            <h4> Name &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;:&nbsp;&nbsp;' . $result["name"] . ' </h4><br>
+            <h4> DOB &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;:&nbsp;&nbsp;' . $result["dob"] . '</h4><br>
+            <h4> College &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;:&nbsp;&nbsp;' . $result["college"] . '</h4><br>
+            <h4> Branch &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;:&nbsp;&nbsp;' . $result["branch"] . '</h4><br> 
+            <h4> PayStatus &nbsp;&nbsp;:&nbsp;&nbsp;' . $pay . '</h4><br> 
         </div>
     </div>    
 </div>
    ';
-  
-   
-    
-   
-    if($paystatus==0)
-    {
+
+
+
+
+if ($paystatus == 0) {
     echo '<div class="right" >
         <input type="button" id="butt1" value="EDIT/REGISTER" onclick="editor()">
     </div>';
-    }
-    echo '</div>
+}
+echo '</div>
     <div id="main">
         <h3 id="text1" >Event Details</h3>
-        <div style="width:90%;height:100%;display:flex;justify-content:center;align-items:center;">
-            <div id="event"> 
-                <div style="margin-top: 35px;">
-                    <ul style="list-style: none; text-decoration: none; font-size: large;">
-                        <h4 style="position: -webkit-sticky;
+        <div style="width:90%;height:100%;display:flex;justify-content:center;align-items:center;flex-direction:column;border-top-right-radius: 15px;
+        border-top-left-radius: 15px;">
+            <div style="width:100%;display:flex;justify-content:center;align-items:center;background-color:#ffe87f;border-top-right-radius: 15px;
+            border-top-left-radius: 15px;">
+                <div id="total_events" class="totalc_div" style="width:100%;height:60px;font-family:Arial, Helvetica, sans-serif;border-top-right-radius: 15px;
+                border-top-left-radius: 15px;">';
+
+                        $stotal = "select count(*) from ser where stdreg='" . $stdreg . "'";
+
+                        $total_event_count = mysqli_query($con, $stotal);
+                        $tev = mysqli_fetch_assoc($total_event_count);
+
+                        $tevs = $tev['count(*)'];
+                        if ($tevs == 0) {
+                            echo "You must register in any of the events...";
+                        } else {
+                            echo "You are enrolled in " . $tevs . " events.";
+                        }
+                echo  '</div>
+            </div>
+            <div id="lowercontainer" style="width:100%;display:flex;justify-content:center;align-items:center;background-color:#ffec97;border-bottom-right-radius: 15px;
+            border-bottom-left-radius: 15px;">
+                <div id="event"> 
+                    <div style="margin-top: 35px;">
+                        <ul style="list-style: none; text-decoration: none; font-size: large;">
+                            <h4 style="position: -webkit-sticky;
                                     position: sticky;
                                     top: 0px; 
-                                    text-align:center ;
+                                    text-align:center;
                                     padding:5%;
                                     background-color:#85aded;
-                                    border-radius:7px;"> 
+                                    border-radius:7px;font-family:Arial, Helvetica, sans-serif;"> 
                                     EVENTS 
-                        </h4>';
+                            </h4>';
+                            $str2 = "select distinct no, name from eventheader a join ser b on a.no=b.even where b.stdreg='" . $stdreg . "'";
+                            $eventts = mysqli_query($con, $str2);
+                            $eventt = null;
+                            echo '<div class="event-list">';
 
+                            while ($eventt = mysqli_fetch_assoc($eventts)) {
+                                $evenum = $eventt['no'];
+                                $dod = $eventt["name"];
+                                echo '<li class="event-item" onclick=get_side_bar(' . $evenum . ');selectEvent(this)>' . $dod . '</li>';
 
-                        $str2 = "select distinct no, name from eventheader a join ser b on a.no=b.even where b.stdreg='". $stdreg ."'";
-                        $eventts = mysqli_query($con, $str2);
-                        $eventt = null;
-                        echo '<div class="event-list">';
-                        
-                        while($eventt = mysqli_fetch_assoc($eventts)){
-                            $evenum = $eventt['no'];
-                            $dod = $eventt["name"];
-                            echo '<li class="event-item" onclick=get_side_bar('.$evenum.');selectEvent(this)>'.$dod.'</li>';
+                            }
 
-                        }
-                            
-                        echo 
-                    '</ul>
+                    echo '</ul>
+                    </div>
                 </div>
-            </div>
-            <div id="total_events" class="side_div" style="text-align:center; padding-top:25px;">';
-
-                $stotal = "select count(*) from ser where stdreg='". $stdreg ."'";
-
-                $total_event_count = mysqli_query($con, $stotal);
-                $tev = mysqli_fetch_assoc($total_event_count);
-
-                $tevs = $tev['count(*)'];
-                if($tevs == 0)
-                {
-                    echo "You must register in any of the events...";
-                }
-                else{
-                echo "You are enrolled in ".$tevs." events.";
-                }
-                echo 
-            '</div>
-            <div id="list_of_events" style="height:100%;" class="side_div" hidden> 
+                <div id="list_of_events" style="height:400px;" class="side_div" hidden> 
+                </div>
             </div>
         </div>
     </div>
@@ -130,35 +130,35 @@ if (isset($_SESSION['last_activity']) && time() - $_SESSION['last_activity'] > 3
     session_unset(); // unset $_SESSION variable for the run-time
     session_destroy(); // destroy session data in storage
     header("Location: index.html"); // redirect to login page
-  }
-  $_SESSION['last_activity'] = time(); // update last activity time stamp
+}
+$_SESSION['last_activity'] = time(); // update last activity time stamp
 
 ?>
 <script>
 
-let selectedEvent = null; 
+    let selectedEvent = null;
 
-function selectEvent(eventItem) {
-    // Deselect the previously selected item 
-    if (selectedEvent) {
-        selectedEvent.classList.remove("selected");
+    function selectEvent(eventItem) {
+        // Deselect the previously selected item 
+        if (selectedEvent) {
+            selectedEvent.classList.remove("selected");
+        }
+
+        // Select the clicked item
+        eventItem.classList.add("selected");
+        selectedEvent = eventItem;
     }
 
-    // Select the clicked item
-    eventItem.classList.add("selected");
-    selectedEvent = eventItem;
-}
-
-// Add a click event listener to the document to deselect the current item when the user clicks anywhere else on the page
-document.addEventListener("click", function (event) {
-    if (selectedEvent && !event.target.classList.contains("event-item")) {
-        selectedEvent.classList.remove("selected");
-        selectedEvent = null;
-    }
-});
+    // Add a click event listener to the document to deselect the current item when the user clicks anywhere else on the page
+    document.addEventListener("click", function (event) {
+        if (selectedEvent && !event.target.classList.contains("event-item")) {
+            selectedEvent.classList.remove("selected");
+            selectedEvent = null;
+        }
+    });
 
 
-    function get_side_bar(e1){
+    function get_side_bar(e1) {
         document.getElementById('total_events').hidden = true;
 
         document.getElementById('list_of_events').hidden = false;
@@ -166,67 +166,69 @@ document.addEventListener("click", function (event) {
         var event_no = e1;
 
         var xhr = new XMLHttpRequest();
-        xhr.open('GET', 'get_evenames.php?name='+event_no, true);
+        xhr.open('GET', 'get_evenames.php?name=' + event_no, true);
 
-        xhr.onload=function(){
+        xhr.onload = function () {
             var responseHTML = this.responseText;
 
-        // Create a container element
-        var container = document.createElement('div');
-        container.style.width = '70%';
-        container.style.height = '100%';
+            // Create a container element
+            // var container = document.createElement('div');
+            // container.style.width = '70%';
+            // container.style.height = '100%';
 
-        // Create a table
-        var table = document.createElement('table');
+            // Create a table
+            // var table = document.createElement('table');
 
-        // Replace &nbsp; with spaces and split the values based on <td> elements
-        var values = responseHTML.replace(/&nbsp;/g, ' ').split('<td>');
+            // Replace &nbsp; with spaces and split the values based on <td> elements
+            var values = responseHTML.replace(/&nbsp;/g, ' ').split('<td>');
+            document.getElementById('list_of_events').innerHTML = '';
+            // Process each value
+            for (var i = 1; i < values.length; i++) {
+                var value = values[i].replace(/<\/?[^>]+(>|$)/g, '').trim(); // Remove HTML tags and trim whitespace
 
-        // Process each value
-        for (var i = 1; i < values.length; i++) {
-            var value = values[i].replace(/<\/?[^>]+(>|$)/g, '').trim(); // Remove HTML tags and trim whitespace
+                if (value !== '') {
+                    // Create a new row and cell for each value
+                    // var row = document.createElement('tr');
+                    // var cell = document.createElement('td');
 
-            if (value !== '') {
-                // Create a new row and cell for each value
-                var row = document.createElement('tr');
-                var cell = document.createElement('td');
+                    // Create a <span> element and set the class
+                    var span = document.createElement('div');
+                    span.textContent = value;
+                    span.className = 'decor'; // Add your CSS class here
+                    span.style.width = '90%';
+                    document.getElementById('list_of_events').appendChild(span);
+                    // cell.appendChild(span);
 
-                // Create a <span> element and set the class
-                var span = document.createElement('span');
-                span.textContent = value;
-                span.className = 'decor'; // Add your CSS class here
-                cell.appendChild(span);
-
-                row.appendChild(cell);
-                table.appendChild(row);
+                    // row.appendChild(cell);
+                    // table.appendChild(row);
+                }
             }
-        }
 
-        // Append the table to the container
-        container.appendChild(table);
+            // Append the table to the container
+            // container.appendChild(span);
 
-        // Set the container as the innerHTML
-        document.getElementById('list_of_events').innerHTML = '';
-        document.getElementById('list_of_events').appendChild(container);
-            
+            // Set the container as the innerHTML
+
+
+
         }
 
         xhr.send();
 
     }
 
-    function exit(){
+    function exit() {
 
         window.location.replace("slogout.php");
     }
 
-    function editor(){
+    function editor() {
         window.location.replace("editor.php");
     }
 
-    function getpass()
-    {
-                
+    function getpass() {
+
     }
 </script>
+
 </html>
