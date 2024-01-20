8c1cd085237a9589c0f46f43982c9d71ee5dbc6b
shivs-12kk <shivkumark76593@gamil.com>
2024-01-20 12:27:19 +0530

    teamreg dn

--- teamRegistration/data.php
+++ teamRegistration/data.php
@@ -11,25 +11,43 @@ if (isset($_GET['mahotsavid']) && isset($_GET['subevent'])) {
     // Create a SQL query to fetch the name based on mahotsavid
     $sql = "SELECT * FROM student WHERE sno = '". $mahotsavid ."'";
     $sql2="SELECT * FROM teamreg where mhid='". $mahotsavid ."' and subevent='". $subevent."'";
-    $sql3="SELECT * FROM ser where mhid='". $mahotsavid ."' and sen= (select no from subeventheader where subname = '". $subevent ."')";
+   // $sql3="SELECT * FROM ser where mhid='". $mahotsavid ."' and sen= (select no from subeventheader where subname = '". $subevent ."')";
 
-    $result = mysqli_query($con, $sql);
+   $result = mysqli_query($con, $sql);
+   if ( mysqli_num_rows($result) > 0){
+   
+    $sql4=" SELECT regno from student where sno= '$mahotsavid' ";
+    $result4 = mysqli_query($con, $sql4);
+    $row1=mysqli_fetch_assoc($result4);
+
+    $sql5=" SELECT * from payment where stdreg = '$row1[regno]'";
+
+  $result5 = mysqli_query($con, $sql5);
+
+  if( $result5 && mysqli_num_rows($result5)==0)
+  $idcount = -2;
+   }
+   
+
+
+    
     $result2 = mysqli_query($con, $sql2);
-    $result3 = mysqli_query($con, $sql3);
+   // $result3 = mysqli_query($con, $sql3);
 
     if ($result2 && mysqli_num_rows($result2) > 0) {
         $idcount = 1;
     }
 
-    if ($result3 && mysqli_num_rows($result3) == 0) {
-        $idcount = -2;
-    }
+    // if ($result3 && mysqli_num_rows($result3) == 0) {
+    //     $idcount = -2;
+    // }
 
     if ($result && mysqli_num_rows($result) > 0) {
         $row = mysqli_fetch_assoc($result);
         $row['alr'] = $idcount;
         echo json_encode($row);
-    } else {
+    } 
+    else {
         $row['alr'] = -1;
         echo json_encode($row);
     }
