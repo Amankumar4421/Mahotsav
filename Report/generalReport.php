81a223b1bea7145a257c26a9064fbf96d596cacd
shivs-12kk <shivkumark76593@gamil.com>
2024-01-22 11:09:28 +0530

    updated

--- Report/generalReport.php
+++ Report/generalReport.php
@@ -242,7 +242,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     }
 
     //for team
-    
+    echo  $subevent_name;
     $sql2="SELECT team_count from subeventheader where subname ='$subevent_name' ";
     $res=mysqli_query($con,$sql2);
     $row=mysqli_fetch_assoc($res);
@@ -265,7 +265,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     if ($result === false) {
         die("Query failed: " . mysqli_error($con));
     }
-}
+
 
 
     echo "<h2>Registered Students for : $subevent_name</h2>";
@@ -331,7 +331,7 @@ else{
         echo "No students registered for the selected subevent.";
     }
 }
-
+}
 
 ?>
 <script>
